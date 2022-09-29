678cf11df77d6a2132091e9b09cea149fc789766
alemedinabjj <101877712+alemedinabjj@users.noreply.github.com>
2022-09-29 10:51:44 -0300

    ajuste

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,8 +2,18 @@ import { BaseIcon } from "@utils/BaseIcon";
 import Image from "next/image";
 import IMG from "../public/teste.png";
 import Link from "next/link";
+import { useState } from "react";
 
 export const Card = () => {
+
+  const [star, setStar] = useState(0);
+
+  const counterStar = () => {
+    setStar(star => star + 1);
+  }
+  
+
+
   return (
     <div className="w-[250px] min-h-[320px] bg-darkprimary dark:bg-primary flex flex-col justify-between rounded">
       <div>
@@ -11,7 +21,7 @@ export const Card = () => {
           <h1 className="text-center text-textdark">Card</h1>
         </header>
         <div className="w-full">
-          <Image src={IMG} width={250} height={167} alt="card pixel" />
+          <Image src={IMG} width={250} height={167} alt="card pixel" objectFit="cover" />
         </div>
       </div>
       <div className="flex items-center gap-10">
@@ -20,8 +30,12 @@ export const Card = () => {
           <p className="text-white">12k views</p>
         </div>
         <div className="flex items-center gap-2 px-3 py-2">
-          <BaseIcon name="FaStar" size="1rem" color="#FFD700" />
-          <p className="text-white">1</p>
+          <button onClick={counterStar} className="flex items-center gap-2"
+            aria-label="Star counter "
+          >
+            <BaseIcon name="FaStar" size="1rem" color="#FFD700" />
+            <p className="text-white">{star}</p>
+          </button>
         </div>
       </div>
       <span
